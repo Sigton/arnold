8e0d8ad0abab1faaedff1a4904ee8469827ab568
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-01-09 16:43:38 +0000

    Create the backend class

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 import tkinter.filedialog as tkfd
 
+
+class ArnoldApp(tk.Tk):
+
+    # This is the main backend class
+
+    def __init__(self):
+
+        # Call the parents constructor
+        tk.Tk.__init__(self)
+
+        # Set the window title
+        tk.Tk.wm_title(self, "Arnold")
+
+        # Set the window size
+        self.geometry('{}x{}'.format(800,600))
